b60def188dd93ded1d06589339cefa8136259fbe
Hoang Nguyen <128159205+BetterCallHoang@users.noreply.github.com>
2026-02-26 18:13:57 +0700

    update singleton.cpp

--- singleton_pattern/SNAP_example_after.cpp
+++ singleton_pattern/SNAP_example_after.cpp
@@ -1,10 +1,13 @@
 /*
-- Chỉ có 1 Logger duy nhất
-
+- Tạo ra một file 1 Logger duy nhất
 - Mọi nơi dùng chung*/
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <mutex>
+#include <chrono>
+#include <ctime>
 
 // Logger sử dụng Singleton Pattern
 class Logger {
@@ -21,14 +24,27 @@ public:
     }
 
     void log(const std::string& message) {
-        std::cout << "[LOG] " << message << std::endl;
+        std::lock_guard <std::muxtex> lock(mutex_); // Tạo mutex để tránh 2 thread cùng ghi vào một thời điểm
+        auto now = std::chrono::system_clock::now();
+        std::time_t time = std::chrono::system_clock::to_time_t(now);
+        file_ << std::ctime(&time) << message << std::endl;
     }
 
 private:
     // Constructor private để không ai tạo được trực tiếp
     Logger() {
-        std::cout << "Logger được tạo duy nhất một lần\n";
+        file_.open("logger.txt", std::ios::app);
+        if(!file_) {
+            throw std::runtime_error("Cannot open log file");
+        } 
+        std::cout << "Logger được tạo duy nhất một lần" << std::endl;
     }
+    ~Logger() {
+        file_.close();
+    }
+private:
+    std::ofstream file_;
+    std::mutex mutex_;
 };
 
 // Module xử lý ảnh
@@ -36,7 +52,7 @@ class ImageProcessor {
 public:
     void process() {
         // Lấy logger chung
-        Logger::getInstance().log("Bắt đầu xử lý ảnh");
+        Logger::getInstance().log("[Image Processor] Bắt đầu xử lý ảnh");
     }
 };
 
@@ -45,7 +61,7 @@ class CalibrationModule {
 public:
     void calibrate() {
         // Dùng cùng một logger
-        Logger::getInstance().log("Bắt đầu hiệu chỉnh");
+        Logger::getInstance().log("[Calibrate] Bắt đầu hiệu chỉnh");
     }
 };
 
